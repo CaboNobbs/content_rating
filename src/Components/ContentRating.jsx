f5768b6cc80128357c885f9a73a4d8186206da10
CaboNobbs <david.delcerro@gmail.com>
2024-09-04 07:01:54 -0400

    Added buttons like and dislike

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,40 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislakes: 0,
+        handleLike:() => {
+            this.setState((prevState) => ({
+                    likes:prevState.likes + 1
+                })
+            )
+        },
+
+        handleDislike:() => {
+            this.setState((prevState) => ({
+                    dislakes:prevState.dislakes + 1
+                })
+            )
+        }
+    };    
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+     <div className='content-rating'>
+        <p>
+            //Add text here
+            Sports
+        </p>
+        <div className='rating-buttons'> 
+            <button className='like-button' onClick={this.state.handleLike}>
+                Like ({this.state.likes})
+            </button>
+            <button className='dislike-button' onClick={this.state.handleDislike}>
+                Dislike ({this.state.dislakes})
+            </button>
+        </div>
+     </div>
     );
   }
 }
